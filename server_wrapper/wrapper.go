c5e61455921ee28315513492572e1060b3ae6ae5
Tobias <yrt1999@163.com>
2021-04-14 17:39:31 +0800

    Format code

--- server_wrapper/wrapper.go
+++ server_wrapper/wrapper.go
@@ -6,25 +6,26 @@ type Middleware interface {
 	Before(writer http.ResponseWriter, request *http.Request)
 	After(writer http.ResponseWriter, request *http.Request)
 }
+
 type ServerWrapper struct {
-	server http.Handler
+	server      http.Handler
 	middlewares []Middleware
 }
 
-func New(s http.Handler)ServerWrapper{
+func New(s http.Handler) ServerWrapper {
 	return ServerWrapper{s, nil}
 }
 
-func (s *ServerWrapper) AddMiddleware(m Middleware){
+func (s *ServerWrapper) AddMiddleware(m Middleware) {
 	s.middlewares = append(s.middlewares, m)
 }
 
-func (s ServerWrapper) ServeHTTP(writer http.ResponseWriter, request *http.Request){
+func (s ServerWrapper) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	for _, m := range s.middlewares {
 		m.Before(writer, request)
 	}
 	s.server.ServeHTTP(writer, request)
-	for i := len(s.middlewares) - 1; i >= 0 ;i--{
+	for i := len(s.middlewares) - 1; i >= 0; i-- {
 		s.middlewares[i].After(writer, request)
 	}
 }
